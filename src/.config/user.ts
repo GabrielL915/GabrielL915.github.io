13d7ee4a2b163a45984d2216dfc59c6d7023f89b
GabrielL915 <gabrielluizgomes1@gmail.com>
2024-10-16 15:09:34 -0300

    add user config

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,4 +4,46 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
+  site: {
+    title: 'Gabriel',
+    subtitle: 'Portifolio Blog',
+    author: 'GabrielL915',
+    description: 'A portifolio',
+    socialLinks: [
+      {
+        name: 'github',
+        href: '',
+      },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+      {
+        name: 'Projects',
+        href: '/projects',
+      },
+    ],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      '<a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">GitHub Repository</a>',
+    ],
+  },
+  appearance: {
+    theme: 'dark',
+    locale: 'en-us',
+  },
 }
